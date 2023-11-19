f2696bc83a0305b574bef6ce4323b596c65e48d7
FUANITO2029 <juanito2029@live.com.mx>
2023-11-18 23:02:52 -0600

    created database frits table

--- Backend/MunchEZ/src/main/java/mx/com/MunchEZ/MunchEZ/MunchEzApplication.java
+++ Backend/MunchEZ/src/main/java/mx/com/MunchEZ/MunchEZ/MunchEzApplication.java
@@ -2,11 +2,13 @@ package mx.com.MunchEZ.MunchEZ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MunchEzApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MunchEzApplication.class, args);
 	}
 
---
+++ Backend/MunchEZ/src/main/resources/db/migration/V1__create-table-products.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+    pro_id INT NOT NULL AUTO_INCREMENT,
+    pro_name VARCHAR(100) NOT NULL,
+    pro_price INT NOT NULL,
+    pro_description VARCHAR(200)
+);
