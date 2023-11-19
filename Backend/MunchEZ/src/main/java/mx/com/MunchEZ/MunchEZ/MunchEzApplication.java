a88e243ad7a1dc286bc11f7839ea9003944d8495
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-11-18 18:35:08 -0600

    Added the principal structure for the backend

---
+++ Backend/MunchEZ/src/main/java/mx/com/MunchEZ/MunchEZ/MunchEzApplication.java
@@ -0,0 +1,13 @@
+package mx.com.MunchEZ.MunchEZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchEzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchEzApplication.class, args);
+	}
+
+}
