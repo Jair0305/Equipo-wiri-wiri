b5096de0cc4d57cf6c871bcc20738846048ad8ed
Naydelin Zavala <123757421+naydelinzavala7@users.noreply.github.com>
2023-11-20 12:25:54 -0600

    feat: getProducts
    feat: getProducts

--- Frontend/src/pages/Desserts.jsx
+++ Frontend/src/pages/Desserts.jsx
@@ -1,8 +1,16 @@
+import { useLoaderData } from 'react-router-dom'
+import { getProducts } from '../api/products'
+import ProductsListDesserts from '../components/ProductsListDesserts'
+
+export async function loaderdesserts() {
+  // traer informacion de la api
+  const products = await getProducts()
+  return products.desserts
+}
 
 const Desserts = () => {
-  return (
-    <div>Desserts</div>
-  )
+  const desserts = useLoaderData()
+  return <ProductsListDesserts desserts={desserts} />
 }
 
 export default Desserts
