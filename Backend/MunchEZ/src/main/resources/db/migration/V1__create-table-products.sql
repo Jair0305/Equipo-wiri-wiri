617ef4277ded2f1d97587956c886b66a91e6b332
FUANITO2029 <juanito2029@live.com.mx>
2023-11-20 14:57:19 -0600

    Before pull main branch

--- Backend/MunchEZ/src/main/resources/db/migration/V1__create-table-products.sql
+++ Backend/MunchEZ/src/main/resources/db/migration/V1__create-table-products.sql
@@ -2,5 +2,6 @@ CREATE TABLE products (
     pro_id INT NOT NULL AUTO_INCREMENT,
     pro_name VARCHAR(100) NOT NULL,
     pro_price INT NOT NULL,
-    pro_description VARCHAR(200)
+    pro_description VARCHAR(200),
+    PRIMARY KEY (pro_id)
 );
