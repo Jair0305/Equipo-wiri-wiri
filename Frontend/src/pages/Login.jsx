f0807236448fc73d9f420a06b2415c888ad9fe10
Diego Pérez <diegoperezperez518@gmail.com>
2023-11-27 14:40:43 -0600

    feat: logic to send user and pass to login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -31,8 +31,19 @@ const Login = () => {
     setIsPasswordFocused(false)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    await fetch('http://127.0.0.1:8080/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
   }
 
   return (
