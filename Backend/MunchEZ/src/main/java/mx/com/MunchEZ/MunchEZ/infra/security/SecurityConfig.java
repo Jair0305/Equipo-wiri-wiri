463a0b9ab70a1a5f2bcd0b9edaf82549ea0a9e87
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-11-27 22:22:11 -0600

    Fix: Request from all

--- Backend/MunchEZ/src/main/java/mx/com/MunchEZ/MunchEZ/infra/security/SecurityConfig.java
+++ Backend/MunchEZ/src/main/java/mx/com/MunchEZ/MunchEZ/infra/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,12 +28,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf ->
-                        csrf
-                .disable())
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-                                .requestMatchers(HttpMethod.POST,"/auth/**").permitAll()
+                                .requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
                         )
                 .formLogin(Customizer.withDefaults());
